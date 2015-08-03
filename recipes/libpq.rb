c65b22773211c40e48fd918b6cb972b75ac45d99
Edward Savage <esavage@zendesk.com>
2015-08-03 15:33:35 +1000

    WIP centos doesn't provide libpq5 easily...
    can only find version 4, this is a bit of a pain

--- recipes/libpq.rb
+++ recipes/libpq.rb
@@ -5,10 +5,13 @@
 
 include_recipe "postgresql"
 
-package "libpq5" do
-  options "-t #{node["postgresql"]["apt_distribution"]}-pgdg"
-end
+case node["platform"]
+when "debian", "ubuntu"
+  package "libpq5" do
+    options "-t #{node["postgresql"]["apt_distribution"]}-pgdg"
+  end
 
-package "libpq-dev" do
-  options "-t #{node["postgresql"]["apt_distribution"]}-pgdg"
+  package "libpq-dev" do
+    options "-t #{node["postgresql"]["apt_distribution"]}-pgdg"
+  end
 end
